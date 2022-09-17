4c1dc136145b914a8aa69f934272b63a85f5fe23
Srb František <srb.frank@gmail.com>
2022-09-17 03:33:58 +0200

    Revert "Revert "bezier yeeeee""
    This reverts commit bee51c75ebbc3cf37c3961daa4c472597f906b36.

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : Singleton<Tower>
+{
+    public List<GameObject> levels = new List<GameObject>();
+
+}
